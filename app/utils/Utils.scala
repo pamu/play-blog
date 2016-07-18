04a5f2ac3650508b788f60cd7d6659d76dd3539b
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-07-19 00:32:08 +0530

    merge

---
+++ app/utils/Utils.scala
@@ -0,0 +1,24 @@
+package utils
+
+import scala.annotation.tailrec
+
+/**
+  * Created by pnagarjuna on 19/07/16.
+  */
+object Utils {
+  def merge[A, B](seq: Seq[(A, B)]): Map[A, Seq[B]] = {
+    @tailrec
+    def helper(rest: Seq[(A, B)], result: Map[A, Seq[B]]): Map[A, Seq[B]] = rest match {
+      case Seq() => result
+      case head +: tail =>
+        val newResult =
+          if (result.contains(head._1)) {
+            result + (head._1 -> (result(head._1) :+ head._2))
+          } else {
+            result + (head._1 -> Seq[B](head._2))
+          }
+        helper(tail, newResult)
+    }
+    helper(seq, Map.empty[A, Seq[B]])
+  }
+}
