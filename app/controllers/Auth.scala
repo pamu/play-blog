dee9143b041254841a401cf51f5f1862f0458368
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-07-27 14:27:58 +0530

    oauth2 action

--- app/controllers/Auth.scala
+++ app/controllers/Auth.scala
@@ -4,17 +4,43 @@ import com.google.inject.Inject
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import services.OAuthServices
-import services.models.State
+import services.models.{RandomSecureString, State}
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
-class Auth @Inject() (oAuthServices: OAuthServices) extends Controller {
+class Auth @Inject()(oAuthServices: OAuthServices) extends Controller {
 
   def login = Action.async { implicit req =>
-    oAuthServices.auth(State(Json.obj("msg" -> "hello"))).map { _ =>
-      Ok("done")
+    Future.successful {
+      Redirect(routes.Auth.oauth2(State(Json.obj("msg" -> "hello"))))
     }
-    //Future.successful(Ok(views.html.login()))
+  }
+
+  implicit class MapConverter(rMap: Map[String, String]) {
+    def convert: List[String] = rMap.map(pair => s"${pair._1}=${pair._2}").toList
+  }
+
+
+  object GOAuthEndpoints {
+    val goauthServiceURL = s"""https://accounts.google.com/o/oauth2/v2/auth"""
+    val clientId = s"""995561758104-2civgrjum3kij0fhj1h836rppi4jqg04.apps.googleusercontent.com"""
+    val clientSecret = s"""rA7Gu4LIuYC2dPYvTUQk0B9x"""
+  }
+
+
+  def oauth2(state: State) = Action { req =>
+    val random = RandomSecureString.getOne
+    val params = Map[String, String](
+      "response_type" -> "token",
+      "client_id" -> GOAuthEndpoints.clientId,
+      "nonce" -> random.str,
+      "redirect_uri" -> "http://rxcode.herokuapp.com/oauth2callback",
+      "scope" -> "email",
+      "state" -> state.toString()
+    ).convert.mkString("?", "&", "").toString
+    val requestURI = s"${GOAuthEndpoints.goauthServiceURL}${params}"
+
+    Redirect(requestURI)
   }
 
 }
--- app/services/OAuthServices.scala
+++ app/services/OAuthServices.scala
@@ -1,43 +1,12 @@
 package services
 
 import com.google.inject.{ImplementedBy, Inject, Singleton}
-import org.apache.http.HttpStatus
 import play.api.libs.ws.WSClient
-import services.models.{RandomSecureString, State}
-import services.statuses.OAuthProcessingDone
-
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 
 @ImplementedBy(classOf[GOAuthServicesImpl])
 trait OAuthServices {
-  def auth(state: State): Future[_]
 }
 
 @Singleton
 class GOAuthServicesImpl @Inject()(wsClient: WSClient) extends OAuthServices {
-  override def auth(state: State): Future[_] = {
-    val random = RandomSecureString.getOne
-    wsClient.url(GOAuthEndpoints.goauthServiceURL).withQueryString(
-      "response_type" -> "token",
-      "client_id" -> GOAuthEndpoints.clientId,
-      "nonce" -> random.str,
-      "redirect_uri" -> "http://rxcode.herokuapp.com/oauth2callback",
-      "scope" -> "email",
-      "state" -> state.toString()
-    ).get().flatMap { wsRes =>
-      val status = wsRes.status
-      if (wsRes.status == HttpStatus.SC_OK) {
-        Future.successful {
-          OAuthProcessingDone(Some(state))
-        }
-      } else Future.failed(new Exception(s"Bad status, status: $status"))
-    }
-  }
-}
-
-object GOAuthEndpoints {
-  val goauthServiceURL = s"""https://accounts.google.com/o/oauth2/v2/auth"""
-  val clientId = s"""995561758104-2civgrjum3kij0fhj1h836rppi4jqg04.apps.googleusercontent.com"""
-  val clientSecret = s"""rA7Gu4LIuYC2dPYvTUQk0B9x"""
 }
