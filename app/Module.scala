a11a7023241299dbe0f0c0d4354158877f34622a
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-07-30 21:42:48 +0530

    table creation on app start

--- app/Module.scala
+++ app/Module.scala
@@ -1,10 +1,12 @@
 import com.google.inject.AbstractModule
+import models.repos.AppStart
 import play.api.{Configuration, Environment}
 
 
 class Module(environment: Environment, configuration: Configuration) extends AbstractModule {
 
   override def configure(): Unit = {
+    bind(classOf[AppStart]).asEagerSingleton()
   }
 
 }
