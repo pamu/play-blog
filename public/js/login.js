ddacc3d3500d85dc103aac600a62feb4ae5fa220
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-08-02 03:57:57 +0530

    position button and preloader in middle

--- public/js/login.js
+++ public/js/login.js
@@ -3,8 +3,9 @@ $(function() {
         $("#gmail_signin").click(function() {
             console.log("gmail signin button clicked");
             window.location="/initiateLogin";
-            $('#gmail_signin').prop('disabled', true);
-            $("#gmail_signin").val('Please wait ...');
+            $("#gmail_signin").html('Please wait ...');
+            $("#gmail_signin").click(function(){});
+            $('#gmail_signin').addClass('disabled');
         });
     }
     sigin();
