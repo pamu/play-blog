53a35673fcf484b0d82bda1eeba2b97d2a8ac9da
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-08-03 02:21:18 +0530

    profile

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,12 @@
 package controllers
 
 import com.google.inject.Inject
-import controllers.mock.MockSecured
 import play.api.Logger
 import play.api.mvc.Controller
 import services.UserServices
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class Application @Inject()(override val userServices: UserServices) extends Controller
   with UserServicesProvider
@@ -15,6 +15,14 @@ class Application @Inject()(override val userServices: UserServices) extends Con
   def index = withUser(parse.anyContent) { user => req =>
     Logger.info("going to index")
     Future.successful(Ok(views.html.index()))
+  }
+
+  def profile = withUser(parse.anyContent) { user => req =>
+    Logger.info("profile action")
+    userServices.getUserInfo(user.email).flatMap {
+      case Some(info) => Future.successful(Ok(views.html.profile(info.picture.link)))
+      case None => Future.successful(Ok(views.html.profile("")))
+    }
   }
 
 }
