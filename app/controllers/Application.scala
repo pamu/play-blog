9a62669b9f04bff18ff07c50f29789e7f1540a3b
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-08-03 03:22:34 +0530

    center image

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,14 +14,15 @@ class Application @Inject()(override val userServices: UserServices) extends Con
 
   def index = withUser(parse.anyContent) { user => req =>
     Logger.info("going to index")
-    Future.successful(Ok(views.html.index()))
+    //Future.successful(Ok(views.html.index()))
+    Future.successful(Redirect(routes.Application.profile))
   }
 
   def profile = withUser(parse.anyContent) { user => req =>
     Logger.info("profile action")
     userServices.getUserInfo(user.email).flatMap {
       case Some(info) => Future.successful(Ok(views.html.profile(info.picture.link)))
-      case None => Future.successful(Ok(views.html.profile("")))
+      case None => Future.successful(Ok(views.html.profile("https://s-media-cache-ak0.pinimg.com/236x/54/bf/a9/54bfa975abbc85378a31fa36f6f7c806.jpg")))
     }
   }
 
